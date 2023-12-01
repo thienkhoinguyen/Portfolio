13ebbae5f485730ecd445f2a4459c625bc3d81e3
Khoi <147691737+thienkhoinguyen@users.noreply.github.com>
2023-12-01 16:34:20 -0700

    added alert button

--- Java.js
+++ Java.js
@@ -1,3 +1,8 @@
 //Footer Current Year//
 var currentYear = new Date().getFullYear();
 document.getElementById("currentYear").innerHTML = currentYear;
+
+// Alert //
+function myFunction() {
+    alert("Does this work?");
+}
