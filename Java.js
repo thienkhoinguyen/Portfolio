67e603cce4b09ca15d83f6e1c3a663f62e3403b8
Khoi <147691737+thienkhoinguyen@users.noreply.github.com>
2023-12-04 20:48:51 -0700

    Added ordered list and increments and includedin console?
    unsure if needed to display for console but added anyways

--- Java.js
+++ Java.js
@@ -1,47 +1,63 @@
-//Footer Current Year//
-var currentYear = new Date().getFullYear();
-document.getElementById("currentYear").innerHTML = currentYear;
+document.addEventListener('DOMContentLoaded', function() {
 
-// Alert //
+    //Footer Current Year//
+    var currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").innerHTML = currentYear;
+  
+    // Alert //
+  
+    const btnAlert = document.getElementById('btn-alert');
+  
+    btnAlert.addEventListener('click', function() {
+        alert('You caught a fish!');
+    });
+  
+    // Hover //
+  
+    const btnHover = document.getElementById('btn-alert');
+  
+    const handleMouseOver = () => {
+        btnHover.innerText = 'Click me';
+    };
+  
+    const handleMouseLeave = () => {
+        btnHover.innerText = 'Click me';
+    };
+  
+    btnHover.addEventListener('mouseover', handleMouseOver);
+    btnHover.addEventListener('mouseleave', handleMouseLeave);
+  
+    // Number Count //
+    let count = 1;
+    const txtCounter = document.getElementById('txt-counter');
+  
+    document.getElementById('btn-counter').onclick = () => {
+        count = count + 1;
+  
+        if (count % 2 === 0) {
+            txtCounter.innerHTML = `Number: ${count}`;
+            txtCounter.style.color = `gold`;
+            txtCounter.classList.remove('odd');
+            txtCounter.classList.add('even');
+        } else {
+            txtCounter.innerHTML = `Number: ${count}`;
+            txtCounter.style.color = `white`;
+            txtCounter.classList.remove('even');
+            txtCounter.classList.add('odd');
+        }
+    };
+  
+    /* Loops */
+  
+    const numbersList = document.getElementById('numbers');
+  
+    for (let i = 1; i <= 100; i++) {
+        const listItem = document.createElement('li');
+        listItem.textContent = i % 2 === 0 ? `even` : `odd`;
+        numbersList.appendChild(listItem);
 
-const btnAlert = document.getElementById('btn-alert');
-
-btnAlert.addEventListener('click', function() {
-    alert('You caught a fish!');
-});
-
-// Hover //
-
-const btnHover= document.getElementById('btn-alert');
-
-const handleMouseOver = () => {
-    btnHover.innerText = 'Click me';
-};
-
-const handleMouseLeave = () => {
-    btnHover.innerText = 'Click me';
-};
-
-btnHover.addEventListener('mouseover', handleMouseOver);
-btnHover.addEventListener('mouseleave', handleMouseLeave);
-
-
-// Number Count //
-let count = 1;
-const txtCounter = document.getElementById('txt-counter');
-
-document.getElementById('btn-counter').onclick = () => {
-    count = count + 1;
-
-    if (count % 2 === 0) {
-        txtCounter.innerHTML = `Number: ${count}`;
-        txtCounter.style.color = `gold`;
-        txtCounter.classList.remove('odd');
-        txtCounter.classList.add('even');
-    } else {
-        txtCounter.innerHTML = `Number: ${count}`;
-        txtCounter.style.color = `white`;
-        txtCounter.classList.remove('even');
-        txtCounter.classList.add('odd');
+        console.log('List Item', i, ':', listItem.textContent);
     }
-};
+  
+  });
+  
